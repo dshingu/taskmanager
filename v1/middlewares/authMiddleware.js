dbd029f80a2f408cd2cac73b4d8725fea0efeb2d
guu876 <dane.shingu@gmail.com>
2023-06-24 21:17:24 -0500

    Updated the auth middleware to check if token is expired

--- v1/middlewares/authMiddleware.js
+++ v1/middlewares/authMiddleware.js
@@ -16,18 +16,18 @@ module.exports = () => {
         if (bearer.toLowerCase() !== 'bearer') return res.status(401).json({message:'Access denied.'});
 
         try {
-            const payload = Jwt.VerifyAccessToken(token);
-            try {
-                const user = await User.findById(payload.uid);
-                req.user = user;
-            } catch (e) {
-                return res.status(401).json({message: 'User not found.'});
+            const payload = Jwt.VerifyAccessToken(token);  
+  
+            if (new Date(payload.exp * 1000) < new Date()) {
+                return res.status(440).json({message: 'Authentication required.'})
             }
             
-            
+            const user = await User.findById(payload.uid);
+            req.user = user;
         }
         catch (error) {
             console.log(error.message);
+            return res.status(401).json({message: 'Access denied.'});
         }
 
         next();
